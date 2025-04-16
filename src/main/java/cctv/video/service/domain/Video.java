af52a37686406d6fcbd041866f191de91911105c
achance6 <ayden.chance@rocketmail.com>
2025-04-16 14:21:48 -0400

    Add validations

--- src/main/java/cctv/video/service/domain/Video.java
+++ src/main/java/cctv/video/service/domain/Video.java
@@ -1,6 +1,7 @@
 package cctv.video.service.domain;
 
 import io.micronaut.core.annotation.NonNull;
+import io.micronaut.core.annotation.Nullable;
 import io.micronaut.serde.annotation.Serdeable;
 
 import java.time.LocalDateTime;
@@ -8,7 +9,8 @@ import java.util.List;
 import java.util.UUID;
 
 @Serdeable
-public record Video(@NonNull UUID uuid, @NonNull String title, String description, List<String> tags,
+public record Video(@NonNull UUID uuid, @NonNull String title, @Nullable String description,
+                    @Nullable List<String> tags,
                     @NonNull LocalDateTime creationDate,
                     @NonNull String uploader) {
 
